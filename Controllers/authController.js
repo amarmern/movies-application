969a72c98119149a14cc3837ee5355a9ce3fe9a2
amarmern <amarmca51@gmail.com>
2024-06-07 17:16:15 +0530

    updateing password functionality

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -187,3 +187,32 @@ exports.resetPassword = asyncErrorHandler(async (req, res, next) => {
     token: loginToken,
   });
 });
+
+exports.updatePassword = asyncErrorHandler(async (req, res, next) => {
+  //GET CURRENT USER DATAT FROM DATABASE
+  const user = await User.findById(req.user._id).select('+password');
+  //CHECK IF THE SUPPLIED CURRENT PASSWORD IS CORRECT
+  if (
+    !(await user.comparePasswordInDb(req.body.currentPassword, user.password))
+  ) {
+    return next(
+      new CustomError('The cuurent password you provided is wrong', 401)
+    );
+  }
+  //IF SUPPLIED PASSWORD IS CORRECT, UPDATE USER PASSWORD WITH NEW VALUE
+
+  user.password = req.body.password;
+  user.confirmPassword = req.body.confirmPassword;
+
+  await user.save();
+
+  //LOGIN USER & SEND JWT
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+});
